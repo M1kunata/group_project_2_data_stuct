9cba3fbe001d739e7bf0e131a6d5038cb7ece89d
Cats_Mix <jakkarinovo@gmail.com>
2026-04-07 10:41:39 +0700

    start

---
+++ main.java
@@ -0,0 +1,13 @@
+package Project2_6713221;
+
+public class main {
+    static public void main(String[] arg)
+    {
+        main mainapp = new main();
+        mainapp.start();
+    }
+    public void start()
+    {
+
+    }
+}
